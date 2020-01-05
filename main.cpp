502c759efae8c8c8373685f7e32ed80fb9258e3e
nongpubin <1050416698@qq.com>
2020-01-05 21:05:05 +0800

    增加json

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include "wrapper.h"
 #include "unistd.h" 
 
+#include "json.h"
+
 
 int main()
 {
 	log_out("hello");
 	log_out(255);
+
+	Json::Value test;
+	test["name"] = "nongpubin";
+	log_out(test["name"].asString());
 	wrapper server(6666,"192.168.88.49",AF_INET);
 }
